9cb5a325e30954a0990561e565a4c31cfb5e0ce4
Alexander white <alexwhite42000@gmail.com>
2016-12-11 15:52:55 -0500

    Made a few QOL changes.

--- package.lisp
+++ package.lisp
@@ -1,3 +1,7 @@
 (defpackage synth
   (:use #:cl)
-  (:export :with-sound :get-byte :sin-sound))
+  (:export :with-sound :get-byte :sin-sound :calc-decay
+           :write-sample :with-sound-file
+           :generator :instrument
+           :make-instrument :seconds
+           :add-generator :tick))
